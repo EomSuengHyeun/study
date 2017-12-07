d051daa7a23453291c6c548ef48f4f8cc884c7c4
big-10 <big-10@BIG-15-PC>
2017-12-07 17:21:31 +0900

    modify GitTest.java : 본체

--- git_push_test/src/com/test/git/GitTest.java
+++ git_push_test/src/com/test/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("와우~~ 내일은 시험보는 날~~!!");
 		System.out.println("내일은 청소도 하셔야해요....");
+		System.out.println("본체와 모니터 퇴실시 끄기");
 	}
 }
